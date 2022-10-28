0e5bc4c158c5feefdeae135882eb27373397e1e2
Surya T N <suryatej.nim@yahoo.com>
2022-10-28 13:17:27 +0000

    Changed CSS class names set for JSX elements and implemented the JSX element tree in VaccinationByGender component

--- src/components/VaccinationByGender/index.js
+++ src/components/VaccinationByGender/index.js
@@ -1,1 +1,35 @@
-// Write your code here
+import {PieChart, Pie, Legend, Cell, ResponsiveContainer} from 'recharts'
+
+import './index.css'
+
+const VaccinationByGender = props => {
+  const {genderBasedData} = props
+
+  return (
+    <div className="vaccination-by-gender-container">
+      <h1 className="vaccination-by-gender-header">Vaccination by gender</h1>
+      <ResponsiveContainer width="100%" height="90%">
+        <PieChart width={1000} height={300}>
+          <Pie
+            dataKey="count"
+            startAngle={180}
+            endAngle={0}
+            data={genderBasedData}
+            cx="50%"
+            cy="50%"
+            outerRadius={80}
+            innerRadius={50}
+          >
+            <Cell name="Male" fill="#f54394" />
+            <Cell name="Female" fill="#5a8dee" />
+            <Cell name="Others" fill="#2cc6c6" />
+          </Pie>
+
+          <Legend iconType="circle" align="center" verticalAlign="bottom" />
+        </PieChart>
+      </ResponsiveContainer>
+    </div>
+  )
+}
+
+export default VaccinationByGender
