b50a8672dcec895c12338b9ec93d82499e83275b
Surya T N <suryatej.nim@yahoo.com>
2022-10-28 19:18:40 +0000

    Added code to format scale of data on the y-axis of bar graph to render CoWIN data in VaccinationCoverage component

--- src/components/VaccinationCoverage/index.js
+++ src/components/VaccinationCoverage/index.js
@@ -13,13 +13,24 @@ import './index.css'
 const VaccinationCoverage = props => {
   const {coverageData} = props
 
+  const CountDataFormatter = countValue => {
+    let formattedCountString = ''
+    if (countValue > 1000) {
+      formattedCountString = `${countValue / 1000}k`
+    } else {
+      formattedCountString = `${countValue}`
+    }
+
+    return formattedCountString
+  }
+
   return (
     <div className="vaccination-coverage-container">
       <h1 className="vaccination-coverage-header">Vaccination Coverage</h1>
       <ResponsiveContainer width="100%" height={500}>
         <BarChart width={1000} height={300} data={coverageData}>
           <XAxis dataKey="vaccineDate" />
-          <YAxis />
+          <YAxis tickFormatter={CountDataFormatter} />
           <Tooltip />
           <Legend />
           <Bar dataKey="dose1" name="Dose1" fill="#5a8dee" />
