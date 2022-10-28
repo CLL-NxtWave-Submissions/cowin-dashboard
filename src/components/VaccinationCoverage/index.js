0d231203726e861621c107976924a03cdc1928ca
Surya T N <suryatej.nim@yahoo.com>
2022-10-28 12:00:50 +0000

    Implemented the JSX elements in VaccinationCoverage component to render the intended bar chart that visualizes CoWIN data in the last 7 days

--- src/components/VaccinationCoverage/index.js
+++ src/components/VaccinationCoverage/index.js
@@ -1,1 +1,33 @@
-// Write your code here
+import {
+  ResponsiveContainer,
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  Tooltip,
+  Legend,
+} from 'recharts'
+
+import './index.css'
+
+const VaccinationCoverage = props => {
+  const {coverageData} = props
+
+  return (
+    <div className="vaccination-coverage-container">
+      <h1 className="vaccination-coverage-header">Vaccination Coverage</h1>
+      <ResponsiveContainer width="100%" height="90%">
+        <BarChart width={1000} height={300} data={coverageData}>
+          <XAxis dataKey="vaccineDate" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Bar dataKey="dose1" fill="#5a8dee" />
+          <Bar dataKey="dose2" fill="#f54394" />
+        </BarChart>
+      </ResponsiveContainer>
+    </div>
+  )
+}
+
+export default VaccinationCoverage
